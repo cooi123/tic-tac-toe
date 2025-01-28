6d2828e95bb6b9338336e343e6140de015393cf3
Vihanga <vihaaanga.mihiranga@gmail.com>
2025-01-28 17:44:51 +1000

    implement gameboard UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import Gameboard from "./components/Gameboard";
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" isCross={true}/>
+          <Player initialName="Player 2" isCross={false}/>
+        </ol>
+        <Gameboard/>
+      </div>
+      MOVES
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function Player({ initialName, isCross }) {
+  // hook for state managing the current/updated playername
+  const [playerName, setPlayerName] = useState(initialName);
+  // hook for state managing 'edit'/'save' output
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    // always use a function to change state when new state depends on previous state
+    setIsEditing((isEditing) => !isEditing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {isEditing ? (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+
+        <span className="player-symbol">{isCross ? "X" : "O"}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
