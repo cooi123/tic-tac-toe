91a52c56148c40771eabcb9d8830bb5064ab1049
Vihanga <vihaaanga.mihiranga@gmail.com>
2025-01-29 13:36:30 +1000

    Implement gameboard state logic for hardcoded 'X' moves

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,18 +1,38 @@
+import { useState } from "react";
+
 const initialGameboard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-  ];
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 export default function Gameboard() {
+  const [prevGameboard, setGameboard] = useState(initialGameboard);
+
+  function handleGameboardState(rowIndex, colIndex) {
+    setGameboard(() => {
+      // make deep copy of board
+      const updatedGameboard = prevGameboard.map((innerArray) => [
+        ...innerArray,
+      ]);
+      // update symbol on the UI tile
+      updatedGameboard[rowIndex][colIndex] = "X";
+      return updatedGameboard;
+    });
+  }
+
   return (
-    <ol id  = 'game-board'>
-      {initialGameboard.map((row, rowIndex) => (
+    <ol id="game-board">
+      {prevGameboard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button
+                  onClick={() => handleGameboardState(rowIndex, colIndex)}
+                >
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
